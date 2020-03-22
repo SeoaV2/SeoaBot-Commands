ebf72402eed41806e106d4ccb41d054e9aee4100
comjun04 <comjun04@gmail.com>
2020-03-22 15:32:57 +0900

    Added Invite Command

---
+++ invite.js
@@ -0,0 +1,17 @@
+const Command = require('../classes/Command.js')
+
+class InviteCommand extends Command {
+  constructor() {
+    super()
+
+    this.name = 'invite'
+    this.description = 'invite bot'
+    this.aliases = ['초대']
+  }
+
+  run(_seoa, msg, args) {
+    msg.channel.send('You can invite **SeoaBot** with this link: \nhttps://discordapp.com/oauth2/authorize?client_id=0000000000&permissions=3072&scope=bot\n(Currently not working)')
+  }
+}
+
+module.exports = InviteCommand
