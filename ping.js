59df6c5a086bd3d991778721110902e4e2fdf296
pmh-only <pmhstudio.pmh@gmail.com>
2020-03-22 13:10:59 +0900

    Ping Command Improvement

--- ping.js
+++ ping.js
@@ -1,15 +1,45 @@
+const ping = require('ping').promise.probe
 const Command = require('../classes/Command')
 
 class PingCommand extends Command {
-  constructor() {
+  constructor () {
     super()
 
     this.name = 'ping'
     this.aliases = ['핑']
   }
 
-  run(seoa, msg, args) {
-    msg.channel.send('pong')
+  run (_seoa, msg, query) {
+    const limit = isNaN(parseInt(query.args[0])) ? 6 : parseInt(query.args[0])
+    const pings = []
+    let count = 1
+
+    msg.channel.send('Idle: ' + (limit / 10) + 's')
+      .then((m) => {
+        const interval = setInterval(pinging, 100)
+        function pinging () {
+          ping('discordapp.com')
+            .then(pingThen)
+            .catch(console.log)
+        }
+
+        function pingThen (res) {
+          count++
+          pings.push(res.time)
+          if (count > limit) {
+            clearInterval(interval)
+            let sum = 0
+            pings.sort((left, right) => left - right)
+            pings.forEach((p) => { sum = Math.round((sum + p) * 100) / 100 })
+
+            const min = Math.round(pings[0] * 100) / 100
+            const max = pings[pings.length - 1]
+            const avg = Math.round(sum / pings.length * 100) / 100
+
+            m.edit('sum/min/avg/max: ' + [sum, min, avg, max].join('/'))
+          }
+        }
+      })
   }
 }
 
