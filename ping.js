a7b726cd01a5185b76e0d37f3d8c91d13beebba7
comjun04 <comjun04@gmail.com>
2020-03-22 13:49:37 +0900

    simpler ping

--- ping.js
+++ ping.js
@@ -1,4 +1,3 @@
-const ping = require('ping').promise.probe
 const Command = require('../classes/Command')
 
 class PingCommand extends Command {
@@ -9,38 +8,9 @@ class PingCommand extends Command {
     this.aliases = ['핑']
   }
 
-  run (_seoa, msg, query) {
-    const request = isNaN(parseInt(query.args[0])) ? 6 : parseInt(query.args[0])
-    const pings = []
-    let count = 1
-
-    if (request > 120) return msg.channel.send('Limit: 120, Request: ' + request)
-    msg.channel.send('Idle: ' + (request / 2) + 's')
-      .then((m) => {
-        const interval = setInterval(pinging, 500)
-        function pinging () {
-          ping('discordapp.com')
-            .then(pingThen)
-            .catch(console.log)
-        }
-
-        function pingThen (res) {
-          count++
-          pings.push(res.time)
-          if (count > request) {
-            clearInterval(interval)
-            let sum = 0
-            pings.sort((left, right) => left - right)
-            pings.forEach((p) => { sum = Math.round((sum + p) * 100) / 100 })
-
-            const min = Math.round(pings[0] * 100) / 100
-            const max = pings[pings.length - 1]
-            const avg = Math.round(sum / pings.length * 100) / 100
-
-            m.edit('sum/min/avg/max: ' + [sum, min, avg, max].join('/'))
-          }
-        }
-      })
+  async run (_seoa, msg, query) {
+    const m = await msg.channel.send('Pinging...')
+    return m.edit(`API: ${Math.round(msg.client.ws.ping)}ms\nComu: ${Math.round(m.createdTimestamp - msg.createdTimestamp)}ms`)
   }
 }
 
