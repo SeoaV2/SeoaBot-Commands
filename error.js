5bc2f8d19611636908a5555a43bd2d9a259089db
comjun04 <comjun04@gmail.com>
2020-03-23 19:27:03 +0900

    Added Error (Testing) Command

---
+++ error.js
@@ -0,0 +1,18 @@
+const Command = require('../classes/Command')
+
+class ErrorCommand extends Command {
+  constructor () {
+    super()
+
+    this.name = 'error'
+    this.deacription = 'Error Test'
+    this.aliases = ['에러']
+    this.ownerOnly = true
+  }
+
+  run (_seoa, _msg, _args) {
+    throw new Error('Test Error')
+  }
+}
+
+module.exports = ErrorCommand
