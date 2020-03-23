c9df00c1aa245027f0bce9256b614501d16f954a
comjun04 <comjun04@gmail.com>
2020-03-23 09:42:44 +0900

    botinfo: Added more information

--- botinfo.js
+++ botinfo.js
@@ -12,9 +12,13 @@ class BotInfoCommand extends Command {
   run (seoa, msg, _args) {
     const embed = new MessageEmbed()
       .setTitle('Information of **Seoa**')
+      .setThumbnail(seoa.user.avatarURL())
       .addFields(
         { name: 'Discord Tag', value: seoa.user.tag, inline: true },
-        { name: 'Command Aliases', value: seoa.commands._commands.size }
+        { name: 'Command Aliases', value: seoa.commands._commands.size, inline: true },
+        { name: 'Servers which I joined', value: seoa.guilds.cache.size, inline: true },
+        { name: 'Channels that I can see', value: seoa.channels.cache.size, inline: true },
+        { name: 'Users that I can recognize', value: seoa.users.cache.size, inline: true }
       )
 
     return msg.channel.send(embed)
