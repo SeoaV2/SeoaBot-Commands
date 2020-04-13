ee668fd561faf4012da1a1d0a337fbb10403cae9
pmh-only <pmhstudio.pmh@gmail.com>
2020-04-13 12:25:44 +0900

    Leaderboard title

--- codeQuiz/leaderboard.js
+++ codeQuiz/leaderboard.js
@@ -16,12 +16,21 @@ class LeaderBoardCommand extends Command {
   run (seoa, msg, _query, locale) {
     const cqExt = seoa.extensions.get('CodeQuiz')
     cqExt.getLeaderboard().then((e) => {
-      const embed = new MessageEmbed().setColor(0xff09b0)
+      const embed = new MessageEmbed()
+        .setColor(0xff09b0)
+        .setTitle(seoa.locale.t('command.codequiz.leaderboard.title:Code Quiz Leaderboard', locale))
+
       let temp = '```fix\n'
       e.forEach((v, i) => {
-        const surfix = ['st', 'nd', 'rd']
-
-        temp += (i + 1) + (surfix[i] || 'th') + ': ' + seoa.users.resolve(v.id).username + ' (' + v.score + ' score)\n'
+        const surfix = ['st', 'nd', 'rd'] // 1st, 2nd, 3rd
+        temp += seoa.locale.t(
+          'command.codequiz.leaderboard.item:%1$s%2$s: %3$s (%4$s score)\n',
+          locale,
+          i + 1,
+          surfix[i] || 'th', // if korean, do not need to use this
+          seoa.users.resolve(v.id).username,
+          v.score
+        )
       })
 
       embed.setDescription(temp + '```')
